4c23350a25d5891e1fced4e4372d29a7934fec4a
salvgit-afk <manninasalvatore434@gmail.com>
2026-02-24 13:35:14 +0100

    modificato portfolio per fare versione più boxata con card

--- functions.js
+++ functions.js
@@ -1,6 +1,7 @@
 document.addEventListener("DOMContentLoaded", () => {
     const titolo = document.querySelector(".titolo-animato");
     const sottotitolo = document.querySelector(".sottotitolo-animato");
+    const paragrafo = document.querySelector(".paragrafo-animato");
 
     setTimeout(() => {
         titolo.classList.add("attivo");
